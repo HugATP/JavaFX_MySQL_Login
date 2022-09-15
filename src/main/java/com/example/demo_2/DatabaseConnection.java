ab8feccfe517ee21101b37255f8d9284631bc60b
ThuyHuong <21020467@vnu.edu.vn>
2022-09-15 17:42:13 +0700

    add getdata function

--- src/main/java/com/example/demo_2/DatabaseConnection.java
+++ src/main/java/com/example/demo_2/DatabaseConnection.java
@@ -11,16 +11,9 @@ public class DatabaseConnection {
     private String databaseUser;
     private String databasePassword;
     public Connection getConnection() {
-        try {
-            FileInputStream fis = new FileInputStream("src/main/resources/config.properties");
-            Properties prop = new Properties();
-            prop.load(fis);
-            databaseUser = prop.getProperty("username");
-            databasePassword = prop.getProperty("password");
-            databaseName =  prop.getProperty("databaseName");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String path = "src/main/resources/config.properties";
+        getDataFromConfigFile(path);
+
         String url = "jdbc:mysql://localhost:3306/" + databaseName;
 
         try {
@@ -30,5 +23,18 @@ public class DatabaseConnection {
             e.printStackTrace();
         }
         return databaseLink;
+    }
+
+    public void getDataFromConfigFile(String path) {
+        try {
+            FileInputStream fis = new FileInputStream(path);
+            Properties prop = new Properties();
+            prop.load(fis);
+            databaseUser = prop.getProperty("username");
+            databasePassword = prop.getProperty("password");
+            databaseName =  prop.getProperty("databaseName");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
