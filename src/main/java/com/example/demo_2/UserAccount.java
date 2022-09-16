0cf36f8b78b959aa670337962cd07debf15703b6
ThuyHuong <21020467@vnu.edu.vn>
2022-09-17 00:45:41 +0700

    lombok

--- src/main/java/com/example/demo_2/UserAccount.java
+++ src/main/java/com/example/demo_2/UserAccount.java
@@ -1,5 +1,11 @@
 package com.example.demo_2;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserAccount {
     private Integer userID;
 
@@ -7,54 +13,4 @@ public class UserAccount {
     private String password;
     private String firstName;
     private String lastName;
-
-
-    public UserAccount() {}
-
-    public UserAccount(Integer userID, String username, String password, String firstName, String lastName) {
-        this.userID = userID;
-        this.username = username;
-        this.password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public Integer getUserID() {
-        return userID;
-    }
-
-    public void setUserID(Integer userID) {
-        this.userID = userID;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.demo_2 {
     requires javafx.fxml;
     requires java.sql;
     requires mysql.connector.java;
+    requires lombok;
 
     opens com.example.demo_2 to javafx.fxml;
     exports com.example.demo_2;
