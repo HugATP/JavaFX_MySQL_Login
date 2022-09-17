33f72d685d39043dc776f9973b5557fc88bd2518
ThuyHuong <21020467@vnu.edu.vn>
2022-09-17 22:24:46 +0700

    btn css

--- src/main/java/com/example/demo_2/AppMain.java
+++ src/main/java/com/example/demo_2/AppMain.java
@@ -3,6 +3,7 @@ package com.example.demo_2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -10,16 +11,14 @@ import java.io.IOException;
 
 public class AppMain extends Application {
     public static FXMLLoader fxmlLoader_userhome = new FXMLLoader(AppMain.class.getResource("login.fxml"));
-    public static Scene scene;
-    //    public static Stage stage = new Stage();
     @Override
     public void start(Stage stage) throws IOException {
 
-        scene = new Scene(fxmlLoader_userhome.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader_userhome.load(), 600, 400);
         stage.setResizable(false);
         stage.initStyle(StageStyle.UNDECORATED);
-//        Image icon_ = new Image("java/img/icons8-login-64.png");
-//        stage.getIcons().add(icon_);
+        /*Image icon_ = new Image("@../icons8-login-64.png");
+        stage.getIcons().add(icon_);*/
         stage.setTitle("Login app");
         stage.setScene(scene);
         stage.show();
